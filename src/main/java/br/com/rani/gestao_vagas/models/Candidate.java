7b4c19adaa7204105e21d6e1b5f33777d77c7f64
Raniere Gadelha <chownrani@users.noreply.github.com>
2026-05-13 14:22:49 -0300

    refactor: candidate entity

--- src/main/java/br/com/rani/gestao_vagas/models/Candidate.java
+++ src/main/java/br/com/rani/gestao_vagas/models/Candidate.java
@@ -1,52 +1,25 @@
 package br.com.rani.gestao_vagas.models;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity(name = "tb_candidate")
-public class Candidate {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    private String name;
+public class Candidate extends AbstractEntity {
 
     @NotBlank
     @Pattern(regexp = "\\S+", message = "Username cannot have any spaces")
+    @Column(unique = true)
     private String username;
-
-    @Email(message = "Email should be valid")
-    private String email;
 
     @Length(min = 8, max = 64)
     private String password;
 
-    private String description;
-
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-    
-    public String getUsername() {
-        return this.username;
-    }
+    private String cpf;
 
 }
 
