68a60962f50078a3864b8f56b33cca6d5b365a47
Raniere Gadelha <chownrani@users.noreply.github.com>
2026-05-14 11:22:47 -0300

    feat: company service

---
+++ src/main/java/br/com/rani/gestao_vagas/services/CompanyService.java
@@ -0,0 +1,29 @@
+package br.com.rani.gestao_vagas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rani.gestao_vagas.exceptions.CompanyAlreadyExistsException;
+import br.com.rani.gestao_vagas.models.Company;
+import br.com.rani.gestao_vagas.repositories.CompanyRepository;
+
+@Service
+public class CompanyService {
+    
+    @Autowired
+    private CompanyRepository repository;
+
+    public Company create(Company company) {
+        repository
+            .findByUsernameOrEmailOrCnpj(
+                company.getUsername(), 
+                company.getEmail(),
+                company.getCnpj())
+            .ifPresent((e) -> {
+                throw new CompanyAlreadyExistsException();
+            });
+            
+        return repository.save(company);
+    }
+
+}
